fda83929ffa964a538d676d0103f18fea508769a
diogocror <diogocror@gmail.com>
2015-05-03 19:39:47 +0000

    cloud9 teste

--- MatrixInterface/MatrixInterface/IMatrix.cs
+++ MatrixInterface/MatrixInterface/IMatrix.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,8 @@ namespace MatrixInterface
 {
     public interface IMatrix
     {
+        private String Teste = "";
+        private List<string> lol = new List<string>();
     }
 }
+

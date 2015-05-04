6059cadb54c99cea643c111313d18d7475da01ba
diogocror <diogocror@gmail.com>
2015-05-04 09:37:57 +0000

    added the author

--- MatrixInterface/MatrixInterface/INode.cs
+++ MatrixInterface/MatrixInterface/INode.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ Author: Diogo Cró
+*/
 namespace MatrixInterface
 {
     public interface INode<T>
